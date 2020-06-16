c659af01966e6e041a0abee3e7a106ee026090fb
josh fernandes <joshfernandes@mac.com>
2020-06-15 23:05:48 -0700

    added CLI arguments

--- imprint.go
+++ imprint.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func printUsage() {
+	fmt.Println("Usage of imprint:")
+	flag.PrintDefaults()
+}
 
 func main() {
-	fmt.Println("hello")
+	boolPtr := flag.Bool("single", false, "a boolean for setting single line")
+	flag.Parse()
+	args := flag.Args()
+	fmt.Println("args:", args)
+	fmt.Println("single:", *boolPtr)
+	fmt.Println("tail:", flag.Args())
+	// fmt.Println(ip)
+	if argLength := len(args); argLength < 1 {
+		printUsage()
+		os.Exit(1)
+	}
+
 }
