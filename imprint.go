f6d76fe5b1719b3a8aa7505f68c5926de124ef8d
josh fernandes <joshfernandes@mac.com>
2020-06-16 22:30:02 -0700

    added ability to make files
    will check to make sure user is not creating directories or same name as directories and then will create the files.

--- imprint.go
+++ imprint.go
@@ -4,11 +4,37 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func printUsage() {
 	fmt.Println("Usage of imprint:")
 	flag.PrintDefaults()
+}
+
+func writeFile(path string) {
+	f, err := os.Create(path)
+	defer f.Close()
+	check(err)
+	_, err = f.WriteString("writes\n")
+	check(err)
+}
+
+func isDirectory(path string) bool {
+	fileInfo, err := os.Stat(path)
+	status := false
+	if err == nil {
+		if fileInfo.Mode()&os.ModeType == os.ModeDir {
+			status = true
+		}
+	}
+	return status
 }
 
 func main() {
@@ -23,5 +49,21 @@ func main() {
 		printUsage()
 		os.Exit(1)
 	}
+	// stuff, _ := filepath.Abs(args[0])
+	// fmt.Println(len(args))
+	// fmt.Println("filepath:", stuff)
+	for i := 0; i < len(args); i++ {
+		currentArg := args[i]
+		_, file := filepath.Split(currentArg)
+		if file == "" || isDirectory(currentArg) {
+			fmt.Println("Only enter filepaths, not directory paths")
+			os.Exit(2)
+		}
+	}
 
+	for i := 0; i < len(args); i++ {
+		file, _ := filepath.Abs(args[i])
+		fmt.Println(file)
+		writeFile(file)
+	}
 }
